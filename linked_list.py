961a0267816e8b63f3297f2af558e0d26a69e60b
Anthony Perez <stockcart21@yahoo.com>
2018-07-04 21:53:20 -0700

    Create linked_list.py
    Implementing other data structures.

---
+++ linked_list.py
@@ -0,0 +1,8 @@
+# A Linked List is used to implement other data structures.
+
+#Implementation
+class Node:
+   def__init__(self, data=None, next=None):
+      self.data = data
+      self.next = next
+      
