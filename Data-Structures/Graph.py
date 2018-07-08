b4f579d6c45689dfc1f5011d506c7568527c37e7
Anthony Perez <stockcart21@yahoo.com>
2018-07-08 14:53:23 -0700

    Create Graph.py
    The code above takes a constructor and nodes and issues to 0

---
+++ Data-Structures/Graph.py
@@ -0,0 +1,19 @@
+# Graphs are used to represent real world applications
+
+# Python implementation
+class Graph():
+   def __init__(self, size):
+      self.adg = []
+      for i in range(1,size+1):
+         self.adj.append([0 for i in range(size)]
+      self.size = size
+      
+      # adding an edge
+      def add_edge(self, orig, dest):
+      
+      #removing an edge
+      def remove_edge(self, orig, dest):
+      
+      #print the matrix
+      def display(self):
+      
