ec1cb4e5860a720595867714ca17b427d278d0ea
Anthony Perez <stockcart21@yahoo.com>
2018-07-07 21:06:42 -0700

    Create Queue.py
    This data-structure uses the FIFO (First in First out) = Method

---
+++ Data-Structures/Queue.py
@@ -0,0 +1,18 @@
+# This data structure allows elements inserted
+
+# Python implementation
+class Queue:
+   def __init__(self):
+      self.items = []
+      
+   def is_empty(self):
+      return self.items == []
+      
+   def enqueue(self, item):
+      self.items.insert(0,item)
+      
+   def dequeue(self):
+      return self.items.pop()
+      
+   def print_queue(self):
+      print(self.items)
