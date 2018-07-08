e4d14888153375cc2bc5346d419296fa91c7e15d
Anthony Perez <stockcart21@yahoo.com>
2018-07-08 15:07:34 -0700

    Create Hash Table.py
    Simple example but there are more complex hash tables out there.

---
+++ Data-Structures/Hash Table.py
@@ -0,0 +1,15 @@
+# Hash tables are data structures where maps and keys to values.
+
+# Python implementation
+class hash_table:
+    def __init__(self):
+      self.table = [None] * 127
+      
+    # Hash function
+    def hash_func(self,value):
+    
+    def Inset(self,value):
+    def Search(self, value):
+    def Remove(self,value):
+    
+    
