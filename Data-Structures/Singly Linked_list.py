a5584eed2643744d437f46a0cf43c25d059c8ccf
Anthony Perez <stockcart21@yahoo.com>
2018-10-04 15:16:10 -0700

    Create Singly Linked_list.py

---
+++ Data-Structures/Singly Linked_list.py
@@ -0,0 +1,26 @@
+# This is a data-structure that holds a group of nodes and numbers that are in sequence together
+
+# Implementation
+
+class Node:
+   def __init__(self, data=None, next=None):
+      self.data = data
+      self.data = data
+   def set_data(self, data):
+      self.data = data
+   def get_data(self):
+      return self.data
+   def set_next(self, next):
+      self.next = next
+   def get_next(self):
+      return self.next
+      
+# defining a single linked list
+class SinglyLinkedList:
+    def __init__(self):
+      self.head = None
+      self.size = 0
+      
+    def add(self, value):
+
+# To be continued...
